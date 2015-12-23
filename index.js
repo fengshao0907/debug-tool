104ca5ef7e958b2d3378e27b1f379a36ce9db1c4
xudafeng <xudafeng@126.com>
2015-12-23 16:26:45 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* ================================================================
+ * debug-tool by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Dec 23 2015 16:01:31 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+module.exports = require('./lib/debug-tool');
---
+++ test/debug-tool.test.js
@@ -0,0 +1,20 @@
+/* ================================================================
+ * debug-tool by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Dec 23 2015 16:01:31 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var debug-tool = require('..');
+
+describe('test', function() {
+
+});
